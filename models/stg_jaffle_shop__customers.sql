014d76a293bee8ff5b4c7ee2ee061281c4284c26
Taofeek  Olawoye <oladipupoolawoye26@gmail.com>
2025-05-02 18:56:16 +0000

    second commit

--- models/stg_jaffle_shop__customers.sql
+++ models/stg_jaffle_shop__customers.sql
@@ -4,14 +4,11 @@
     )
 }}
 
-with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
 
-    from raw.jaffle_shop.customers
+select
+    id as customer_id,
+    first_name,
+    last_name
+from raw.jaffle_shop.customers
 
-)
-SELECT * FROM raw.jaffle_shop.customers
